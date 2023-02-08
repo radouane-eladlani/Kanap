db8b18f8f74e5bfadc2a2eff3c698b4eb068a138
radouane <radouane.eladlani@gmail.com>
2023-02-08 10:37:38 +0100

    modification  script + ajout localstorage

--- front/js/product.js
+++ front/js/product.js
@@ -1,25 +1,25 @@
-/* Récupération de l'id et savoir lequel des diferents produits de l'API a afficher*/
+/* Récupération de l'id et savoir lequel des differents produits de l'API a afficher*/
 const url = new URL(document.location);
 const searchParams = url.searchParams;
 const lienId = searchParams.get('id')
-/* Récupération des données de l'API avec l'id du produit*/
+/* Récupération des données d'un produit de l'API avec l'id du produit*/
 fetch(`http://localhost:3000/api/products/${lienId}`)
-  .then((res) => res.json())
-  .then((res) => donnees(res)) 
-/* afficher des éléments produit dans la page produit*/
-function donnees(canape) {
-  const altTxt = canape.altTxt
-  const colors = canape.colors
-  const description = canape.description
-  const imageUrl = canape.imageUrl
-  const  name = canape.name
-  const price = canape.price
-  
-creerImage(imageUrl, altTxt)
-creerTitre(name)
-creerPrix(price)
-creerDescription(description)
-creerColors(colors)
+    .then((res) => res.json())
+    .then((res) => afficherProduit(res))
+/* creer et afficher des éléments produit dans la page produit*/
+function afficherProduit(canape) {
+    const altTxt = canape.altTxt
+    const colors = canape.colors
+    const description = canape.description
+    const imageUrl = canape.imageUrl
+    const name = canape.name
+    const price = canape.price
+    /* on appel les fonctions pour afficher les elements html du produit*/
+    creerImage(imageUrl, altTxt)
+    creerTitre(name)
+    creerPrix(price)
+    creerDescription(description)
+    creerColors(colors)
 }
 /* afficher le titre avec la Création de l'img*/
 function creerImage(imageUrl, altTxt) {
@@ -27,34 +27,44 @@ function creerImage(imageUrl, altTxt) {
     image.src = imageUrl
     image.alt = altTxt
     const parent = document.querySelector(".item__img")
-    if (parent != null) parent.appendChild(image)
+    /* si le parent est different de null alors il va ajouter l'image dans le parent*/
+    if (parent != null) {
+        parent.appendChild(image)
+    }
 }
 /* afficher titre avec la recuperation du id #title*/
-function creerTitre(name){
+function creerTitre(name) {
     const h1 = document.querySelector("#title")
-    if (h1 != null) h1.textContent = name
+    if (h1 != null) {
+        h1.textContent = name
+    }
 }
 /* afficher le prix avec la recuperation du id #price*/
-function creerPrix (price) {
-const span = document.querySelector("#price")
-if (span != null) span.textContent = price
+function creerPrix(price) {
+    const span = document.querySelector("#price")
+    if (span != null) {
+        span.textContent = price
+    }
 }
 /* afficher la description avec la recuperation du id #description*/
 function creerDescription(description) {
     const p = document.querySelector("#description")
-    if (p != null) p.textContent = description
+    if (p != null) {
+        p.textContent = description
+    }
 }
-/* Recuperation du id #colors*/
-/* si il est pas egaux Executer une boucle pour afficher les options colors */
+/* Recuperation du #colors*/
+/* si il est different de null (select) alors on va creer une option pour chaque couleur*/
 /* append option pour afficher les options colors*/
-function creerColors(colors){
+function creerColors(colors) {
     const select = document.querySelector("#colors")
-    if (select != null){
-        colors.forEach(colors => {
-        const option = document.createElement("option")
-        option.value = colors
-        option.textContent = colors 
-        select.appendChild(option) 
+    if (select != null) {
+        /* creer une option pour chaque couleur puis l'ajouter dans le select*/
+        colors.forEach(color => {
+            const option = document.createElement("option")
+            option.value = color
+            option.textContent = color
+            select.appendChild(option)
         })
     }
 }
@@ -62,21 +72,29 @@ const button = document.querySelector("#addToCart")
 /* chaque evenement click (ajouter au panier) 
 on va recuperer la couleur et la quantité du canape */
 button.addEventListener("click", (e) => {
-const color = document.querySelector("#colors").value
-const quantity = document.querySelector("#quantity").value
-/*si colors egale a null ou bien quantity alors afficher le msg d'alerte*/
-if (color == null || color == "" || quantity == null || quantity == "0") {
-    alert("selectionnez une couleur et une quantité")}
-    return 
-}) 
-const donnees = { 
-    id: lienId, 
-    color: colors, 
-    quantity: number(quantity) }
+    const color = document.querySelector("#colors").value
+    const quantity = document.querySelector("#quantity").value
+    /*si colors egale a null ou bien quantity alors afficher le msg d'alerte*/
+    if (color == null || color === "" || quantity == null || quantity == "0") {
+        alert("selectionnez une couleur et une quantité")
+    }
+    /* sinon on va ajouter le canape dans le panier*/
+    else {
+        ajouterAuPanier(color, quantity)
+    }
+})
+function ajouterAuPanier(color, quantity){
+    const donnees = {
+        id: lienId,
+        color: color,
+        quantity: quantity
+    }
+/*stocker les donnees dans le localstorage puis 
+convertir les donnees en chaine json*/
+window.localStorage.setItem(lienId, JSON.stringify(donnees))
 
-    /*stocker les donnees dans le localstorage puis convertir les donnees en chaine json*/
-    window.localStorage.setItem(lienId, JSON.stringify(donnees))
-    /* ensuite on rajoute l'URL cart.html(panier).
-    Au clique sur (ajouter au panier) nous redirigera dans cart.html(panier) avec les "donnees" */
-    window.location.href = "cart.html"
+/*Au clique sur (ajouter au panier) nous redirigera dans
+ cart.html(panier) qui va recuperer les "donnees" dans le localstorage*/
+window.location.href = "cart.html"
+}
 
--- front/js/script.js
+++ front/js/script.js
@@ -1,36 +1,38 @@
 /* recuperer les donnees de l'api des canapes*/
 fetch("http://localhost:3000/api/products")
-.then((res) => res.json())
+/* recuperer les donnees de la reponse de l'API des canape*/
+.then((res) => {
+    return res.json()
+})
+/*ensuite fait appel a la fonction ajoutProduct avec les donnee pour creer 
+les article qui va contenir id,img,h3,p */
 .then((donnees) => ajoutProducts(donnees))
- /*  Il va recuperer les donnees ensuite il va creer un id, article,img,h3,p */
+ /* il va parcourir les donnees ensuite il va creer un id, article,img,h3,p*/
 function ajoutProducts(donnees) {
     donnees.forEach((canape) => {
-        /* Pour chaque canape on va creer un article pour afficher le id du produit avec ,img,h3,p */
+        /* Pour chaque canape on va creer un article pour afficher
+         le id du produit avec ,img,h3,p */
         const { _id, imageUrl, altTxt, name, description } = canape
         const lienId = creerlienId(_id)
         const article = document.createElement("article")
         const image = creerImage(imageUrl, altTxt)
         const h3 = creerH3(name)
         const p = creerParagraphe(description)
-/*  Refactoring.Il va ajouter les elements dans l'article*/
-        appendElementsDeArticle(article,[image,h3,p])
-        appendArticleDelienId(lienId, article)  
+        /* recuperer les balises (image,h3,p) qui vont s'ajouter une par une dans la balise article*/
+        article.appendChild(image)
+        article.appendChild(h3)
+        article.appendChild(p) 
+        appendArticleDelienId(lienId, article)
 })
-}
-/* Executer/afficher chaque element du tableau de l'article de la function appendElementsDeArticle
-puis append l'article dans items */
-function appendElementsDeArticle(article, array) {
-    array.forEach((items) => { article.appendChild(items)
-    
-})
-}
-/* creer un id qui renvoie vers la page produit ( product.html +id) du canape*/
+}    
 function creerlienId(_id){
+    /* creer une balise a pour aller dans la page produit au click*/
     const lienId = document.createElement("a")
     lienId.href = "./product.html?id=" + _id 
     return lienId
 }
-/* recuperer l'id et l'article et les rattacher a #items*/
+/* fait appel a la fonction aADI ensuite recuperer l'id "items" 
+et les rattacher items avec lienId et lienId a article*/
 function appendArticleDelienId(lienId, article) {
 const items = document.querySelector("#items")
 if (items != null) {
@@ -41,24 +43,27 @@ if (items != null) {
 /* creer une image avec un url et un alt*/
  function creerImage(imageUrl, altTxt){
     const image = document.createElement("img")
+    /* ajouter le lien vers l'image dans la balise "img"*/
     image.src = imageUrl
+    /* ajouter texte alternatif dans la balise "img"*/
     image.alt = altTxt
-    image.removeAttribute("title")
-    image.removeAttribute("style")
     return image
  }
- /* creer un nom de canape avec un "h3" ensuite ajouter un textContent */
- /*et la classe productName grace a la methode classlist.add*/
+ /* creer un nom de canape avec un "h3" 
+ ensuite ajouter un innerHTML pour le contenu du name dans le paragraphe 
+ et ajouter la classe "productName" grace a la methode classlist.add*/
  function creerH3(name) {
     const h3 = document.createElement("h3")
-    h3.textContent = name
+    h3.innerHTML = name
     h3.classList.add("productName")
     return h3
  }
- /* creer une description avec un "p"*/
+ /* creer une description de l'article avec la balise "p" 
+ ensuite ajouter un innerHTML pour le contenu du description dans le paragraphe 
+ et ajouter la classe "productdescription" grace a la methode classlist.add*/
  function creerParagraphe(description){
     const p = document.createElement("p")
-    p.textContent = description
+    p.innerHTML = description
     p.classList.add("procuctdescription")
     return p 
  }
